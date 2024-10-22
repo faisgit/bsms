90a8baa3dea1c3df00b002b361acd420ca3f0c75
Faisal Ansari <102345373+faisgit@users.noreply.github.com>
2024-10-22 15:52:40 +0530

    push

--- backend/index.js
+++ backend/index.js
@@ -10,21 +10,24 @@ app.use(cors())
 
 app.use(express.json());
 
-
-app.get("/books/", async(req, res) => {
-    try {
-            const books = await Book.find({});
-            res.status(200).json({
-              sucess: true,
-              data: books,
-            });
-            res.send("Express on vercel")
-          } catch (error) {
-            console.error(`Error in fetch products : ${error.message}`);
-          }
+app.get("/", (req, res) => {
+  res.status(200).send("Express on vercel")
 })
 
-app.post("/books/", async (req, res) => {
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    res.status(200).json({
+      sucess: true,
+      data: books,
+    });
+    res.send("Express on vercel")
+  } catch (error) {
+    console.error(`Error in fetch products : ${error.message}`);
+  }
+})
+
+app.post("/books", async (req, res) => {
   const book = req.body;
 
   if (!book.name || !book.author || !book.price) {
@@ -34,59 +37,59 @@ app.post("/books/", async (req, res) => {
     });
   }
 
-  const newBook =  new Book(book)
+  const newBook = new Book(book)
 
 
   try {
-        await newBook.save();
-        res.status(201).json({
-          sucess: true,
-          data: newBook,
-        });
-      } catch (error) {
-        console.error("Error is Created Product");
-        res.status(500).json({
-          success: false,
-          message: "Server Error",
-        });
-      }
+    await newBook.save();
+    res.status(201).json({
+      sucess: true,
+      data: newBook,
+    });
+  } catch (error) {
+    console.error("Error is Created Product");
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
 });
 
 app.delete("/books/:id", async (req, res) => {
-    const { id } = req.params;
-      console.log(id);
-      try {
-        await Book.findByIdAndDelete(id);
-        res.status(200).json({
-          success: true,
-          message: "Product has been Deleted",
-        });
-      } catch (error) {
-        res.status(400).json({
-          success: false,
-          message: "Product not found",
-        });
-      }
+  const { id } = req.params;
+  console.log(id);
+  try {
+    await Book.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "Product has been Deleted",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
 })
 
 app.put("/books/:id", async (req, res) => {
-    const {id} = req.params
-    const book = req.body;
-    try {
-        const updateBook = await Book.findByIdAndUpdate(id, book,{
-            new : true,
-        })
+  const { id } = req.params
+  const book = req.body;
+  try {
+    const updateBook = await Book.findByIdAndUpdate(id, book, {
+      new: true,
+    })
 
-        res.status(200).json({
-            success : true,
-            data : updateBook
-        })
-    } catch (error) {
-        res.status(500).json({
-            success : false,
-            message : "Server Error"
-        })
-    }
+    res.status(200).json({
+      success: true,
+      data: updateBook
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error"
+    })
+  }
 })
 
 app.listen(5000, () => {
